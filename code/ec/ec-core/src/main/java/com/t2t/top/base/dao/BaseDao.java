4067795bb3a77358b1b586145a515030c191da41
liurr <jrunner@126.com>
2015-11-05 21:38:36 +0800

    modify

---
+++ code/ec/ec-core/src/main/java/com/t2t/top/base/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.t2t.top.base.dao;
+
+/**
+ * Created by Administrator on 2015/11/3.
+ */
+public interface BaseDao {
+}
---
+++ code/ec/ec-core/src/main/java/com/t2t/top/base/service/impl/BaseServiceImpl.java
@@ -0,0 +1,9 @@
+package com.t2t.top.base.service.impl;
+
+import com.t2t.top.base.service.BaseService;
+
+/**
+ * Created by yangpengfei 2015/11/3.
+ */
+public class BaseServiceImpl implements BaseService {
+}
